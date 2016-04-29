eeafb8908cccf99b164669bdb4d6b3273d0d60f0
lizMillerCoder12 <millerElizabeth1991@gmail.com>
2016-04-28 19:11:42 -0700

    Adding Test Main

---
+++ Test/TestingMain.java
@@ -0,0 +1,15 @@
+/**
+ * Created by lizmiller on 4/28/16.
+ */
+
+import org.junit.Test;
+
+
+public class TestingMain {
+
+        @Test
+        public void testMain() {
+            String [] array = new String[3];
+            Main.main(array);
+        }
+}
