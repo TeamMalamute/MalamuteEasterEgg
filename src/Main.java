11861f917f19271fd15655d0cbc4dee9c086cbc3
lizMillerCoder12 <millerElizabeth1991@gmail.com>
2016-04-28 21:11:47 -0700

    Fixing Team and updating main

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /**
  * Created by lizmiller on 4/28/16.
  */
@@ -6,7 +8,9 @@ public class Main {
     public static void main(String[]args) {
         System.out.println("We are Team Malamute and this is our Easter Egg");
 
-        System.out.print(Team.liz());
+        System.out.println(Arrays.toString(Team.getNames()));
+
+
 
     }
 }
